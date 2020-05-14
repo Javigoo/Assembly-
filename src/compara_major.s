81c7dd1e03ffd75a38d5ad44aa9fc274e20dc169
javigoo <javierroiggregorio@gmail.com>
2020-05-14 21:22:34 +0200

    Increases the number of values greater than X

--- src/compara_major.s
+++ src/compara_major.s
@@ -14,15 +14,21 @@ RES:    .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
         .text
 main:   daddui  R1,R0,10        ; R1 = 10  Counter
         dadd    R2,R0,R0        ; R2 = 0   Pointer
-        dadd    R3,R0,R0        ; R3 = 0   Result
+        dadd    R3,R0,R0        ; R3 = 0   Num
+        ld      R6,X(R0)        ; R6 = X   Value
+        ld      R7,RES(R0)        ; R6 = RES Result
 
 loop:   ld      R4,A(R2)        ; R4 = A[R2]    Get value of A - (Iterate A)
 
-        dadd    R3,R3,R4        ; Operacion a realizar - (Suma los elementos de A y los guarda en R3)
+        slt     R5,R6,R4        ; If A[i]>X, R5=1
+        beqz    R5,skip         ; If R5 equal 0 go to skip
 
-        daddi   R2,R2,8         ; R2 = R2+8  Increment pointer - (1 byte)
+        daddi   R3,R3,1         ; NUM++ Increases the number of values greater than X
+        ;sd      R3,NUM(R0)     ; RES = R3 Save result
+
+skip:   daddi   R2,R2,8         ; R2 = R2+8  Increment pointer - (One byte)
         daddi   R1,R1,-1        ; R1 = R1-1  Decrement counter
-        bnez    R1,loop         ;
+        bnez    R1,loop         ; If counter not equal 0 go to loop
   
         sd      R3,NUM(R0)      ; RES = R3 Save result
 
