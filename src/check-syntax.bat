2d6407e603536f874819b2dd591a17db784b0964
javigoo <javierroiggregorio@gmail.com>
2020-05-14 14:59:57 +0200

    Skeleton compara_major.s

--- src/check-syntax.bat
+++ src/check-syntax.bat
@@ -2,6 +2,7 @@
 
 echo "Check syntax for compara_major.s"
 pause
+echo.
 
 ..\winmips64\asm compara_major.s
 del compara_major.dat compara_major.cod
@@ -9,6 +10,8 @@ echo.
 
 echo "Check syntax for compara_major_delay.s"
 pause
+echo.
+
 ..\winmips64\asm compara_major_delay.s
 del compara_major_delay.dat compara_major_delay.cod
 echo.
--- src/compara_major.s
+++ src/compara_major.s
@@ -1,17 +1,16 @@
-;*************************************** 
-;*** winMIPS64 //sum.s//   C=A+B   ***** 
-;*** (c) 2003 CA226, DCU           ***** 
-;***************************************      
+;******************************************************************************************
+;*** Recorre un vector d’enters A de 10 posicions, i determina quants valors són majors ***
+;*** que un valor donat X. El nombre de valors majors que X serà emmagatzemat en una    ***
+;*** variable NUM de memòria. El programa ha de generar a més un altre vector RES, 		***			
+;*** de 10 posicions, tal que RES[i]=1 si A[i]>X o bé RES[i]=0 si A[i]≤X.      			***
+;******************************************************************************************
 
 		.data 
-A:  	.word 10 
-B:    	.word 8 
-C:    	.word 0      
+A: 		.word 10, 2, 9, 12, 43, 152, 2, 4, 18, 3 
+X: 		.word 20 
+NUM: 	.word 0 
+RES: 	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0      
 
 		.text 
-main:      
-		ld r4,A(r0)      
-		ld r5,B(r0)      
-		dadd r3,r4,r5      
-		sd r3,C(r0)      
+main:           
 		halt     
